1b346efea8a3e021a3e48a6933ba4da4f2577818
Dennis Hawran <dennishawran@gmail.com>
2023-01-10 02:05:07 +0100

    Night

--- Assets/Scripts/Nekromancer/SkillData.cs
+++ Assets/Scripts/Nekromancer/SkillData.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
+[Serializable]
 public class SkillData : ScriptableObject
 {
     public string id = "";
