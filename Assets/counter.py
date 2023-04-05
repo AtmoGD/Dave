62d065f76c8d602e292a8a9016a3abb872db03c6
Dennis Hawran <dennishawran@gmail.com>
2023-04-05 15:13:48 +0200

    Updated counter script

--- Assets/counter.py
+++ Assets/counter.py
@@ -1,28 +1,30 @@
 import os
 
-folder_path = './Assets/Scripts'
+directory = os.getcwd()
+folder_path = os.path.join(directory, 'Scripts')
 
 # Initialize counters
 file_count = 0
 total_line_count = 0
 total_word_count = 0
 
-# Iterate through the files in the folder
-for filename in os.listdir(folder_path):
-    # Check if the file has a .cs extension
-    if filename.endswith('.cs'):
-        file_count += 1
-        file_path = os.path.join(folder_path, filename)
+# Recursively walk through the directories and subdirectories
+for root, _, files in os.walk(folder_path):
+    for filename in files:
+        # Check if the file has a .cs extension
+        if filename.endswith('.cs'):
+            file_count += 1
+            file_path = os.path.join(root, filename)
 
-        with open(file_path, 'r', encoding='utf-8') as file:
-            lines = file.readlines()
-            line_count = len(lines)
-            total_line_count += line_count
+            with open(file_path, 'r', encoding='utf-8') as file:
+                lines = file.readlines()
+                line_count = len(lines)
+                total_line_count += line_count
 
-            for line in lines:
-                words = line.split()
-                word_count = len(words)
-                total_word_count += word_count
+                for line in lines:
+                    words = line.split()
+                    word_count = len(words)
+                    total_word_count += word_count
 
 print(f"Total .cs files: {file_count}")
 print(f"Total line count: {total_line_count}")
